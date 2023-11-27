034bd9c523ea1c3f5af55e433edc0fb4eae09d52
TesaTec <magnushende@gmail.com>
2023-11-27 15:21:27 +0100

    Update BuildingManagementSystem.java

--- WorkshopV2/src/Domain/BuildingManagementSystem.java
+++ WorkshopV2/src/Domain/BuildingManagementSystem.java
@@ -8,18 +8,19 @@ public class BuildingManagementSystem {
     ArrayList<Building> buildings = new ArrayList<Building>();
 
     public ArrayList<Building> getBuildings() {
-
+        return buildings;
     }
     public UUID addBuilding(String name) {
-
+       buildings.add(new Building(name));
+       return buildings.getLast().getId();
     }
     public void removeBuilding(UUID id) {
         for(int i = 0; i < buildings.size(); i++) {
-            if(buildings.== id) {
+            if(buildings.get(i).getId() == id) {
                 buildings.remove(i);
                 return;
             }
         }
-        System.out.println("There wasn't a building which matched with the provided id");
+        System.out.println("There was not a building associated with the given id");
     }
 }
